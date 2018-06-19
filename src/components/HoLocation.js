ed3b7dc0539a0337687dc1ba6f2227c512ad394d
Edouard de Laage <edouarddelaage@gmail.com>
2018-06-19 10:44:24 +0200

    Cv Page Wcs Improvement

--- src/components/HoLocation.js
+++ src/components/HoLocation.js
@@ -13,16 +13,16 @@ const HoLocation = () => {
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className="DescHoLoc">
           <div className="Ho">
             <p>Juin 2010 - Décembre 2017</p>
-            <h2 className="HoTitle">Gérant de la société H.O. Location</h2>
-            <h3>Entreprise de location de tentes et matériel   évènementiel composé de 4 salariés CDI.</h3>
+            <h2 className="HoTitle">Gérant de la société H.O. Location</h2>
+            <h3>Entreprise de location de tentes et matériel   évènementiel composé de 4 salariés CDI.</h3>
           </div>
           <div>
             <ul>
-              <li>Développement du CA de 190 000 € à 490 000 €. </li>
-              <li>Gestion financière : Gestion de la   trésorerie / des factures clients et   fournisseurs / comptabilité et clôtures de  bilan.</li>
+              <li>Développement du CA de 190 000 € à 490 000 €. </li>
+              <li>Gestion financière : Gestion de la   trésorerie / des factures clients et   fournisseurs / comptabilité et clôtures de  bilan.</li>
               <li>Gestion des RH : Recrutement et gestion de  CDD en saison (jusqu’a 7).</li>
               <li>Gestion de la logistique : Organisation des   plannings de montage et
-                  démontage. Optimisation des transports.  Coordination des équipes de monteurs.</li>
+                  démontage. Optimisation des transports.  Coordination des équipes de monteurs.</li>
             </ul>
           </div>
         </Grid>
--- src/components/WildCodeSchool.js
+++ src/components/WildCodeSchool.js
@@ -1,44 +1,75 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
 
 import './WildCodeSchool.css'
 
 const WildCodeSchool = () => {
   return (
-    <div container className="Wcs">
-      <div id="WildCodeSchool" className="WildCodeSchool">
-        <div className="HeadWCS">
-          <p>Février 2018 - Juillet 2018</p>
-          <h2 className="WCSTitle">Formation developpeur web Javascript</h2>
-          <h3>Node JS / React / Redux</h3>
-        </div>
-        <p>La Wild Code School vise à former au métier de développeur web. Spécialisation Javascript React / Node JS en 5 mois.</p>
-        <div>
-          <p>Le déroulement de la formation se décompose en 3 temps :</p>
-          <div className="DeroulementFormation">
-            <div className="Project">
-              <h4>Création d’un site internet statique / responsive</h4>
-              <p>(HTML / CSS, Frameworks FRONT-END / Bootstrap / Material Design)</p>
+    <div>
+      <div className="WildCodeSchool Wcs">
+        <Grid container spacing={0} className="HoLoc">
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} id="HoLocation" className="ImgHo">
+            <div className="HeadWCS">
+              <p>Février 2018 - Juillet 2018</p>
+              <h2 className="WCSTitle">Formation developpeur web Javascript </h2>
+              <h3>Node JS / React / Redux</h3>
             </div>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className=" DescHoLoc">
+            <div>
+              <p>La Wild Code School vise à former au métier de   développeur web. Spécialisation Javascript React / Node JS  en 5 mois.</p>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div>
+        <Grid container spacing={0} className="HoLoc">
+
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className="  DescHoLoc">
+            <div className="DeroulFormation">
+              <p>Le déroulement de la formation se décompose en 3 temps :</p>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={12} lg={4} xl={4} className="  DescHoLoc">
+            <div className="Project Project_1">
+              <h4>Création d’un site internet statique / responsive</h4>
+              <p>(HTML / CSS, Frameworks FRONT-END / Bootstrap / Material   Design)</p>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={12} lg={4} xl={4} className="DescHoLoc">
             <div className="Project">
               <h4>Création d’une application web dynamique (Client / API)</h4>
-              <p>Persistance de données (mocks et MySQL) Back : Node JS avec une interface CRUD<br/>
-                    Front : Javascript / HTMLS CSS / Bootstrap<br/>
-                    Projet : création d’une application web type annuaire des anciens élèves de la Wild Code School.<br/>
-                    Fonctionnalités : Inscription et enregistrement de compte en base / Connexion et déconnexion / Modification de profil / Suppression de compte</p>
+              <p>Persistance de données (mocks et MySQL) Back : Node JS   avec une interface CRUD<br/>
+                  Front : Javascript / HTMLS CSS / Bootstrap<br/>
+                  Projet : création d’une application web type annuaire   des anciens élèves de la Wild Code School.<br/>
+                  Fonctionnalités : Inscription et enregistrement de  compte en base / Connexion et déconnexion /  Modification de profil / Suppression de compte</p>
             </div>
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={12} lg={4} xl={4} className="DescHoLoc">
             <div className="Project">
               <h4>Réalisation d’un projet plus complexe pour un client.</h4>
               <p>Base de données : MySQL<br/>
-                    Back : Node JS<br/>
-                    Front : React / Redux.<br/>
-                    Fonctionnalités : <br/>
-                    Zone administrable : création/édition/suppression de contenu / upload de documents / CRUD sur les documents uploadés / sélection de l’article ‘a la une’ / sélection  desX      articles à faire apparaître dans le carrousel de l’accueil / modifier l’identifiant/mdp du compte membre.<br/>
-                    Gestion utilisateurs / rôles : Connexion accès membres (ouvre à des droits supplémentaires consultation/téléchargement.)</p>
+                  Back : Node JS<br/>
+                  Front : React / Redux.<br/>
+                  Fonctionnalités : <br/>
+                  Zone administrable : création/édition/suppression de  contenu / upload de documents / CRUD sur les documents   uploadés / sélection de l’article ‘a la une’ /  sélection  desX      articles à faire apparaître dans  le carrousel de l’accueil / modifier l’identifiant/mdp   du compte membre.<br/>
+                      Gestion utilisateurs / rôles : Connexion accès  membres (ouvre à des droits supplémentaires  consultation/téléchargement.)</p>
             </div>
-          </div>
-          <p>Au total et en 5 mois j’aurais créé 3 projets concrets avec l’aide d’un logiciel de version (Git)</p>
-          <p>Outre l’apprentissage de ces technologies, nous travaillons en groupe de 5 à 6 personnes. <br/>
-                 J’ai donc appris à travailler avec une approche Agile et une méthodologie SCRUM :
+          </Grid>
+
+        </Grid>
+      </div>
+
+      <div className="EndWcs">
+        <Grid container spacing={0} className="HoLoc">
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className="  DescHoLoc">
+            <p>Au total et en 5 mois j’aurais créé 3 projets concrets avec  l’aide d’un logiciel de version (Git)</p>
+            <p>Outre l’apprentissage de ces technologies, nous travaillons  en groupe de 5 à 6 personnes.</p>
+            <p>J’ai donc appris à travailler avec une approche Agile et   une méthodologie SCRUM :</p>
             <ul>
               <li>Désignation d’un Scrum Master</li>
               <li>Réalisation d’un Product Backlog</li>
@@ -46,8 +77,8 @@ const WildCodeSchool = () => {
               <li>Revue de Sprint</li>
               <li>Rétrospective de Sprint.</li>
             </ul>
-          </p>
-        </div>
+          </Grid>
+        </Grid>
       </div>
     </div>
   )
