4a8ab70ae4743f57e57b3804c3405536e3805f49
Edouard de Laage <edouarddelaage@gmail.com>
2018-06-28 07:43:57 +0200

    Improvment css

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import HeaderCv from './HeaderCv'
 import WildCodeSchool from './WildCodeSchool'
 import HoLocation from './HoLocation'
 import Kdovino from './Kdovino'
 import Nav from './Nav'
+import Footer from './Footer'
+import EndFooter from './EndFooter'
+
+import './Cv.css'
 
 const Cv = () => {
   return (
     <div>
-      <Nav />
+      <div className="background_img_cv">
+        <Nav />
+        <HeaderCv />
+      </div>
       <WildCodeSchool />
       <HoLocation />
       <Kdovino />
+      <Footer />
+      <EndFooter />
     </div>
   )
 }
---
+++ src/components/HeaderCv.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './HeaderCv.css'
+
+const HeaderCv = () => {
+  return (
+    <div className="Header">
+      <div className="HeaderContentCV">
+      </div>
+    </div>
+  )
+}
+
+export default HeaderCv
