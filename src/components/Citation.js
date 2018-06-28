2bd209edb2a9e25e5fa8d79ebca8bf5395618d9a
Edouard de Laage <edouarddelaage@gmail.com>
2018-06-28 08:23:55 +0200

    Citation improvment css

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -4,11 +4,14 @@ import './Citation.css'
 const Citation = () => {
   return (
     <div id="CeSite" className="CeSite">
-      <div className="ParaCeSite">
-        <h2 className="TitleCeSite">J'adore créer des sites internet, les faire évoluer et les maintenir</h2>
-        <div>
+      <div className="layer">
+        <div className="ParaCeSite">
+          <h2 className="TitleCeSite">J'adore créer des sites internet, les faire évoluer et les maintenir</h2>
+          <div>
+          </div>
+          <p>Vous avez un projet de création de site ou de refonte de votre site existant ?</p>
+          <p className="contacter">N'hésitez pas a me contacter...</p>
         </div>
-        <p>Vous avez un projet de création de site ou de refonte de votre site existant ?</p>
       </div>
     </div>
   )
