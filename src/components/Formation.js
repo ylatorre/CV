8fc47c3fbe03f61ffee5017d7fe5686f93e49083
Edouard de Laage <edouarddelaage@gmail.com>
2018-07-04 18:16:33 +0200

    Fix linter

--- src/components/Formation.js
+++ src/components/Formation.js
@@ -7,13 +7,13 @@ const Formation = () => {
       <div className="ParaFormation">
         <h2 className="FormationTitle">Compétences complémentaires</h2>
         <h3>Informatique :</h3>
-          <p>Pratique courante : Word, Excel, Power-Point / Environnement Windows et Mac</p>
-          <p>Bonne connaissances : Photoshop, Adobe Premier, CMS Wordpress</p>
+        <p>Pratique courante : Word, Excel, Power-Point / Environnement Windows et Mac</p>
+        <p>Bonne connaissances : Photoshop, Adobe Premier, CMS Wordpress</p>
         <h3>Formation :</h3>
-          <p>2000 - 2003 : IDRAC BTS Action commerciale</p>
-          <p>1999 - 2000 : Baccalauréat ES</p>
+        <p>2000 - 2003 : IDRAC BTS Action commerciale</p>
+        <p>1999 - 2000 : Baccalauréat ES</p>
         <h3>Centre d'intérêts :</h3>
-          <p>Nouvelles téchnologie, photo, footing, cuisine</p>
+        <p>Nouvelles téchnologie, photo, footing, cuisine</p>
       </div>
     </div>
   )
